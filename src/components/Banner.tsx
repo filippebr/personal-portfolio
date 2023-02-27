f84286c26ec07de20ca8359b89df00ae61f4daa6
Filippe Braga <filippeffx@hotmail.com>
2023-02-26 22:26:59 -0300

    ts Particles working

---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+import { Design } from './Design'
+
+function Banner() {
+  return (
+    <section id="home" className="w-full h-[700px]">
+      <Design />
+    </section>
+  )
+}
+
+export default Banner
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bodyFont: "Nunito Sans",
+      },
+      backgroundImage: {
+        "hero-image": "url('/assets/bg.jpg')",
+        "contact-image": "url('/assets/contact.jpg')"
+      },
+      colors: {
+        designColors: "#F7D449",
+        textColor: "#AEAEAE"
+      },
+      boxShadow: {
+        cardShadow: "0px 0px 5px 10px rgba(0,0,0,0.1)",
+      }
+    },
+  },
+  plugins: [],
+}

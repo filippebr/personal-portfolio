ef21ee3de72f605c5638b4169d46aefbf3340af6
Filippe Braga <filippeffx@hotmail.com>
2023-08-27 21:13:32 -0300

    contact form component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@
 import { motion } from 'framer-motion'
 import contactData from 'public/data/contactData'
 import { FormEvent, useState } from 'react'
+import FormContact from './formContact'
 
 function Contact() {
   const [username, setUsername] = useState('')
@@ -11,7 +12,7 @@ function Contact() {
   const [successMsg, setSuccessMsg] = useState('')
 
   const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+    e.preventDefault();    
   
     if (!username) {
       setErrMsg('Please enter your name.');
@@ -60,39 +61,47 @@ function Contact() {
               {successMsg}
             </motion.p>
           ) : (
-            <form className="w-full flex flex-col items-center gap-4 md:gap-10">
-              <div className="w-full flex flex-col md:flex-row items-center gap-4 md:gap-10">
-                <input
-                  onChange={(e) => setUsername(e.target.value)}
-                  placeholder="Name"
-                  className="w-full md:w-1/2 py-2 px-4 text-base text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200"
-                  type="text"
-                />
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Email"
-                  className="w-full md:w-1/2 py-2 px-4 text-base text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200"
-                  type="email"
-                />
-              </div>
+            <FormContact 
+              setUsername={setUsername} 
+              setEmail={setEmail} 
+              setMessage={setMessage} 
+              errMsg={errMsg} 
+              handleSubmit={handleSubmit} 
+            />
 
-              <textarea
-                onChange={(e) => setMessage(e.target.value)}
-                className="w-full text-lg h-42 p-4 text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200 resize-none"
-                placeholder="Your Messages"
-              ></textarea>
-              {errMsg && (
-                <p className="w-full bg-red-500 py-1 text-center text-base font-semibold tracking-wider">
-                  {errMsg}
-                </p>
-              )}
-              <button
-                onClick={handleSubmit}
-                className="w-44 h-12 text-black bg-yellow text-base uppercase font-bold tracking-wide border-2 border-transparent hover:bg-black hover:text-white hover:border-yellow duration-300"
-              >
-                Submit Now
-              </button>
-            </form>
+            // <form className="w-full flex flex-col items-center gap-4 md:gap-10">
+            //   <div className="w-full flex flex-col md:flex-row items-center gap-4 md:gap-10">
+            //     <input
+            //       onChange={(e) => setUsername(e.target.value)}
+            //       placeholder="Name"
+            //       className="w-full md:w-1/2 py-2 px-4 text-base text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200"
+            //       type="text"
+            //     />
+            //     <input
+            //       onChange={(e) => setEmail(e.target.value)}
+            //       placeholder="Email"
+            //       className="w-full md:w-1/2 py-2 px-4 text-base text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200"
+            //       type="email"
+            //     />
+            //   </div>
+
+            //   <textarea
+            //     onChange={(e) => setMessage(e.target.value)}
+            //     className="w-full text-lg h-42 p-4 text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200 resize-none"
+            //     placeholder="Your Messages"
+            //   ></textarea>
+            //   {errMsg && (
+            //     <p className="w-full bg-red-500 py-1 text-center text-base font-semibold tracking-wider">
+            //       {errMsg}
+            //     </p>
+            //   )}
+            //   <button
+            //     onClick={handleSubmit}
+            //     className="w-44 h-12 text-black bg-yellow text-base uppercase font-bold tracking-wide border-2 border-transparent hover:bg-black hover:text-white hover:border-yellow duration-300"
+            //   >
+            //     Submit Now
+            //   </button>
+            // </form>
           )}
           <div className="w-full py-10 bg-black bg-opacity-40 flex flex-col md:flex-row px-4 md:items-center justify-between">
             <div className="text-lg font-thin">
---
+++ src/components/formContact.tsx
@@ -0,0 +1,55 @@
+import { MouseEventHandler } from "react"
+
+type FormContactProps = {
+  setUsername: (username: string) => void
+  setEmail: (email: string) => void
+  setMessage: (message: string) => void
+  errMsg: string
+  handleSubmit: (MouseEventHandler<HTMLButtonElement>)
+}
+
+function FormContact({ 
+  setUsername, 
+  setEmail, 
+  setMessage, 
+  errMsg, 
+  handleSubmit 
+}: FormContactProps): JSX.Element {
+  return (
+    <form className="w-full flex flex-col items-center gap-4 md:gap-10">
+      <div className="w-full flex flex-col md:flex-row items-center gap-4 md:gap-10">
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Name"
+          className="w-full md:w-1/2 py-2 px-4 text-base text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200"
+          type="text"
+        />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          className="w-full md:w-1/2 py-2 px-4 text-base text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200"
+          type="email"
+        />
+      </div>
+
+      <textarea
+        onChange={(e) => setMessage(e.target.value)}
+        className="w-full text-lg h-42 p-4 text-black placeholder:text-gray-600 font-bold placeholder:font-normal outline-none border-transparent border-2 focus-within:border-yellow duration-200 resize-none"
+        placeholder="Your Messages"
+      ></textarea>
+      {errMsg && (
+        <p className="w-full bg-red-500 py-1 text-center text-base font-semibold tracking-wider">
+          {errMsg}
+        </p>
+      )}
+      <button
+        onClick={handleSubmit}
+        className="w-44 h-12 text-black bg-yellow text-base uppercase font-bold tracking-wide border-2 border-transparent hover:bg-black hover:text-white hover:border-yellow duration-300"
+      >
+        Submit Now
+      </button>
+    </form>
+  )
+}
+
+export default FormContact

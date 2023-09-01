9b05669c79e7bb7401168a5ea54e10f770fd1dae
Filippe Braga <filippeffx@hotmail.com>
2023-08-31 22:35:00 -0300

    new images blender design

--- public/assets/index.js
+++ public/assets/index.js
@@ -4,30 +4,28 @@ import bgThree from "../assets/bedroom_03.jpg"
 import bgFour from "../assets/bedroom_04.jpg"
 
 // Blender
-import imgBlender01 from '../assets/blender/abstract_image_crossmind.png'
-import imgBlender02 from '../assets/blender/baroque_ornament_elephant.png'
-import imgBlender03 from '../assets/blender/combat_knife.png'
-import imgBlender04 from '../assets/blender/cosmetic_mockup_derek.png'
-import imgBlender05 from '../assets/blender/crystal.png'
-import imgBlender06 from '../assets/blender/death_planet.png'
-import imgBlender08 from '../assets/blender/diamond.png'
-import imgBlender07 from '../assets/blender/diamond_ring.png'
-import imgBlender09 from '../assets/blender/geonodes_fireball.jpg'
-import imgBlender10 from '../assets/blender/geonodes_flower.png'
-import imgBlender11 from '../assets/blender/geonodes_fractal_cube.png'
-import imgBlender12 from '../assets/blender/leaves_ornament.png'
-import imgBlender13 from '../assets/blender/mitchells_cap.png'
-import imgBlender14 from '../assets/blender/pepper_container.png'
-import imgBlender15 from '../assets/blender/quadcopter.png'
-import imgBlender16 from '../assets/blender/samurai_demon_helmet.jpg'
-import imgBlender17 from '../assets/blender/shoe_adidas_skate.png'
-import imgBlender18 from '../assets/blender/silver_mirror.png'
-import imgBlender19 from '../assets/blender/skull_piston_wrench_marble.png'
-import imgBlender20 from '../assets/blender/spider_yuji.jpg'
-import imgBlender21 from '../assets/blender/staff_energy.png'
-import imgBlender22 from '../assets/blender/the_one_ring_lotr_default_cube.png'
-import imgBlender23 from '../assets/blender/venus_milk_way.png'
-import imgBlender24 from '../assets/blender/wine_bottle_derek.png'
+import imgBlender01 from '../assets/blender/abstract_image_crossmind.jpg'
+import imgBlender02 from '../assets/blender/baroque_ornament_elephant.jpg'
+import imgBlender03 from '../assets/blender/combat_knife.jpg'
+import imgBlender04 from '../assets/blender/cosmetic_mockup_derek.jpg'
+import imgBlender05 from '../assets/blender/death_planet.jpg'
+import imgBlender06 from '../assets/blender/diamond.jpg'
+import imgBlender07 from '../assets/blender/diamond_ring.jpg'
+import imgBlender08 from '../assets/blender/geonodes_fireball.jpg'
+import imgBlender09 from '../assets/blender/geonodes_fractal_cube.jpg'
+import imgBlender10 from '../assets/blender/leaves_ornament.jpg'
+import imgBlender11 from '../assets/blender/mitchells_cap.jpg'
+import imgBlender12 from '../assets/blender/pepper_container.jpg'
+import imgBlender13 from '../assets/blender/quadcopter.jpg'
+import imgBlender14 from '../assets/blender/samurai_demon_helmet.jpg'
+import imgBlender15 from '../assets/blender/shoe_adidas_skate.jpg'
+import imgBlender16 from '../assets/blender/silver_mirror.jpg'
+import imgBlender17 from '../assets/blender/skull_piston_wrench.jpg'
+import imgBlender18 from '../assets/blender/spider_yuji.jpg'
+import imgBlender19 from '../assets/blender/staff_energy.jpg'
+import imgBlender20 from '../assets/blender/the_one_ring_lotr_default_cube.jpg'
+import imgBlender21 from '../assets/blender/venus_milk_way.jpg'
+import imgBlender22 from '../assets/blender/wine_bottle_derek.jpg'
 
 export { bgOne, bgTwo, bgThree, bgFour }
 export {
@@ -35,7 +33,7 @@ export {
   imgBlender06, imgBlender07, imgBlender08, imgBlender09, imgBlender10,
   imgBlender11, imgBlender12, imgBlender13, imgBlender14, imgBlender15,
   imgBlender16, imgBlender17, imgBlender18, imgBlender19, imgBlender20,
-  imgBlender21, imgBlender22, imgBlender23, imgBlender24
+  imgBlender21, imgBlender22
 }
 
 

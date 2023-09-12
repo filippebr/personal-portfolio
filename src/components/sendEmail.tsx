cc0464dfd6dac403cbcd6d624adac9f6e9de35ac
Filippe Braga <filippeffx@hotmail.com>
2023-09-12 20:58:38 -0300

    install resend

--- src/components/sendEmail.tsx
+++ src/components/sendEmail.tsx
@@ -1,5 +1,9 @@
 "use server"
 
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
 export const sendEmail = async (formData: FormData) => {
   console.log("Running on server")
   console.log(formData.get("senderEmail"))

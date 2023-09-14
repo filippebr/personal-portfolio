735ecde0a217561d37a1d8fbcf5ebeda48644ec8
Filippe Braga <filippeffx@hotmail.com>
2023-09-14 18:04:00 -0300

    route resend api send email

--- src/app/api/welcome/route.ts
+++ src/app/api/welcome/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { Resend } from 'resend'
 
-import Welcome from '../emails/Welcome'
+import Welcome from '../../../emails/Welcome'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 

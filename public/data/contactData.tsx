f219ac1f965e44ef821b07c9238c65c99375e34e
Filippe Braga <filippeffx@hotmail.com>
2023-09-21 20:26:06 -0300

    phone icon removed

--- public/data/contactData.tsx
+++ public/data/contactData.tsx
@@ -1,20 +1,20 @@
-import { FaEnvelope, FaMap, FaPhoneAlt } from 'react-icons/fa'
+import { FaEnvelope, FaMap } from 'react-icons/fa'
 
 const ContactData = [
   {
-    id: '1',
+    id: '0',
     icon: <FaMap className="text-4xl text-yellow" />,
     label: 'Location',
     value: 'São Paulo, Brasil',
   },
+  // {
+  //   id: '1',
+  //   icon: <FaPhoneAlt className="text-4xl text-yellow" />,
+  //   label: 'Phone',
+  //   value: '+9999999999',
+  // },
   {
-    id: '2',
-    icon: <FaPhoneAlt className="text-4xl text-yellow" />,
-    label: 'Phone',
-    value: '+9999999999',
-  },
-  {
-    id: '3',
+    id: '1',
     icon: <FaEnvelope className="text-4xl text-yellow" />,
     label: 'Email',
     value: 'webdevfibr@protonmail.com',
